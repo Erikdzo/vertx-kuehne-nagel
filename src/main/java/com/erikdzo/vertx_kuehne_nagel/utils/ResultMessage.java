fa1e15f5377c4db0025e1f4b3716533cd84f43fc
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2020-01-27 12:49:25 +0200

    Add error String

--- src/main/java/com/erikdzo/vertx_kuehne_nagel/utils/ResultMessage.java
+++ src/main/java/com/erikdzo/vertx_kuehne_nagel/utils/ResultMessage.java
@@ -3,4 +3,5 @@ package com.erikdzo.vertx_kuehne_nagel.utils;
 public class ResultMessage {
   public static final String URL = "url";
   public static final String BODY_SIZE = "bodySize";
+  public static final String ERROR = "error";
 }
